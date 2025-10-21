c72a2324f909802d0d26f448251a5f4ba349e105
GeorgeBotos03 <gbotos80@yahoo.ro>
2025-10-21 18:53:43 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ toggleButton.addEventListener('click', function(){
 });
     
 });
+
+// change navbar background on scroll
+
+window.addEventListener('scroll', function(){
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 0){
+        navbar.classList.add('navbar-scroll')
+    } else{
+        navbar.classList.remove('navbar-scroll')
+    }});
